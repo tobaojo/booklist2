2b1c867b880972c2faa6050f388f3906ae53ef34
Toba Ojo <toba@Tobas-MacBook-Air.local>
2020-08-15 22:01:38 +0100

    updated a few bits

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 
 //get values from input
 const bookForm = document.querySelector('#book-form').addEventListener('submit' , (e) => {
-
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
+  const book = new Book(title, author,isbn);
+  const ui = new UI();
 
-const book = new Book(title, author,isbn);
+  //validate
+  if (title || author || isbn === '') {
+    checkFields();
+  } else {
+    //add book to List
+    ui.addBookToList(book);
+
+    ui.clearFields();
+  }
 
 
 
-const ui = new UI();
-
-//add book to List
-
-ui.addBookToList(book);
-
-ui.clearFields();
   e.preventDefault();
 });
 
@@ -26,22 +28,24 @@ function Book(title, author, isbn){
   this. author = author;
   this.isbn = isbn;
 }
+
 //UI Constructor
+function UI(){
+
+}
+
 
 UI.prototype.addBookToList = function (book) {
-  const list = document.querySelector('#book-list');
-
-  const row = document.createElement('tr');
-
-//insert col
-row.innerHTML = `<td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.isbn}</td>
-                <td><a href='#' class = "delete">X</a></td>`
-
-
-list.appendChild(row);
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+  //insert col
+  row.innerHTML = `<td>${book.title}</td>
+                  <td>${book.author}</td>
+                  <td>${book.isbn}</td>
+                  <td><a href='#' class = "delete">X</a></td>`
+  list.appendChild(row);
 };
+
 
 UI.prototype.clearFields = function () {
   document.querySelector('#title').value = '';
@@ -49,6 +53,10 @@ UI.prototype.clearFields = function () {
   document.querySelector('#isbn').value = '';
 };
 
-function UI(){
-
+function checkFields(){
+  const msg = document.createElement('div')
+  msg.classList.add('error');
+  msg.innerHTML = 'Please fill in fields'
+  const container = document.querySelector('.container')
+  container.appendChild(msg);
 }
