c34e04858b2936832d26c88fac0d502dab216b22
Toba Ojo <toba@Tobas-MacBook-Air.local>
2020-08-15 14:22:37 +0100

    started functionality, good place to stop

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const isbn = document.querySelector('#isbn')
+
+//get values from input
+const bookForm = document.querySelector('#book-form').addEventListener('submit' , (e) => {
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+const book = new Book(title, author,isbn);
+
+
+
+const ui = new UI();
+
+//add book to List
+
+ui.addBookToList(book);
+
+ui.clearFields();
+  e.preventDefault();
+});
+
+//book constructor
+function Book(title, author, isbn){
+  this.title = title;
+  this. author = author;
+  this.isbn = isbn;
+}
+//UI Constructor
+
+UI.prototype.addBookToList = function (book) {
+  const list = document.querySelector('#book-list');
+
+  const row = document.createElement('tr');
+
+//insert col
+row.innerHTML = `<td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.isbn}</td>
+                <td><a href='#' class = "delete">X</a></td>`
+
+
+list.appendChild(row);
+};
+
+UI.prototype.clearFields = function () {
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+  document.querySelector('#isbn').value = '';
+};
+
+function UI(){
+
+}
